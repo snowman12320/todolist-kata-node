43a29587ae851353c77a01b00ea9630d179ea98e
DESKTOP-QIDODUT\acer <snowman12320@gmail.com>
2024-02-23 17:31:16 +0800

    Refactor server.js and add Thunder Client collection

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const errHandle = require('./errorHandle');
 const todos = [
   {
     title: 'todo 1',
-    id: '1',
+    id: uuidv4(),
   },
   {
     title: 'todo 2',
-    id: '2',
+    id: uuidv4(),
   },
   {
     title: 'todo 3',
-    id: '3',
+    id: uuidv4(),
   },
 ];
 
@@ -64,7 +64,61 @@ const requestListener = (req, res) => {
         errHandle(res);
       }
     });
+  } else if (req.url == '/todos' && req.method == 'DELETE') {
+    todos.length = 0;
+    res.writeHead(200, headers);
+    res.write(
+      JSON.stringify({
+        status: 'success',
+        data: todos,
+      })
+    );
+    res.end();
+  } else if (req.url.startsWith('/todos/') && req.method == 'DELETE') {
+    try {
+      const id = req.url.split('/').pop();
+      const index = todos.findIndex((todo) => todo.id === id);
+      if (index !== -1) {
+        todos.splice(index, 1);
+        res.writeHead(200, headers);
+        res.write(
+          JSON.stringify({
+            status: 'success',
+            data: todos,
+          })
+        );
+        res.end();
+      } else {
+        errHandle(res);
+      }
+    } catch (error) {
+      errHandle(res);
+    }
+  } else if (req.url.startsWith('/todos/') && req.method == 'PATCH') {
+    req.on('end', () => {
+      try {
+        const id = req.url.split('/').pop();
+        const title = JSON.parse(body).title;
+        const index = todos.findIndex((todo) => todo.id === id);
+        if (index !== -1 && title !== undefined) {
+          todos[index].title = title;
+          res.writeHead(200, headers);
+          res.write(
+            JSON.stringify({
+              status: 'success',
+              data: todos,
+            })
+          );
+          res.end();
+        } else {
+          errHandle(res);
+        }
+      } catch (error) {
+        errHandle(res);
+      }
+    });
   } else if (req.method == 'OPTIONS') {
+    console.log('options', req.method, req.url);
     res.writeHead(200, headers);
     res.end();
   } else {
